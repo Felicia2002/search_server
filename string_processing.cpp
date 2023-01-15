6d0f1ebb1542e0c4a90d237cc5d7ba3fb03d1d3c
Надежда Чернигина <97780261+Felicia2002@users.noreply.github.com>
2023-01-15 15:28:40 +0300

    Create string_processing.cpp

---
+++ string_processing.cpp
@@ -0,0 +1,25 @@
+#include "string_processing.h"
+
+using std::string;
+using std::vector;
+
+
+#include "string_processing.h"
+
+std::vector<std::string_view>
+    SplitIntoWords(std::string_view str) {
+    std::vector<std::string_view> result;
+    const int64_t pos_end = str.npos;
+    while (true) {
+        int64_t space = str.find(' ');
+        result.push_back(space == pos_end ? str.substr() :
+                         str.substr(0, space));
+        if (space == pos_end) {
+            break;
+        } else {
+            ++space;
+            str.remove_prefix(space);
+        }
+    }
+    return result;
+}
