bb36f061fd09fd62e6548f50d327227bfa8de9b2
Надежда Чернигина <97780261+Felicia2002@users.noreply.github.com>
2023-01-21 12:00:27 +0300

    Update document.cpp

--- document.cpp
+++ document.cpp
@@ -1,6 +1,7 @@
 #include "document.h"
 
-Document::Document() = default;
+using std::ostream;
+using namespace std::string_literals;
 
 Document::Document(int id, double relevance, int rating)
     : id(id)
@@ -8,28 +9,10 @@ Document::Document(int id, double relevance, int rating)
     , rating(rating) {
 }
 
-std::ostream& operator<<(std::ostream & out, const Document & document) {
+ostream& operator<<(ostream& out, const Document& document) {
     out << "{ "s
         << "document_id = "s << document.id << ", "s
         << "relevance = "s << document.relevance << ", "s
         << "rating = "s << document.rating << " }"s;
     return out;
-}
-
-void PrintDocument(const Document & document) {
-    std::cout << "{ "s
-        << "document_id = "s << document.id << ", "s
-        << "relevance = "s << document.relevance << ", "s
-        << "rating = "s << document.rating << " }"s << std::endl;
-}
-
-void PrintMatchDocumentResult(int document_id, std::vector<std::string_view>words, DocumentStatus status) {
-    std::cout << "{ "s
-        << "document_id = "s << document_id << ", "s
-        << "status = "s << static_cast<int>(status) << ", "s
-        << "words ="s;
-    for (std::string_view word : words) {
-        std::cout << ' ' << word;
-    }
-    std::cout << "}"s << std::endl;
 }
