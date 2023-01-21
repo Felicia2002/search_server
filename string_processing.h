a7570c77726261c79c8ff09f2e75aabc3466a2b1
Надежда Чернигина <97780261+Felicia2002@users.noreply.github.com>
2023-01-21 12:13:00 +0300

    Update string_processing.h

--- string_processing.h
+++ string_processing.h
@@ -3,20 +3,16 @@
 #include <string>
 #include <vector>
 #include <set>
-#include <string_view>
 
-std::vector<std::string_view>
-SplitIntoWords(std::string_view str);
+std::vector<std::string> SplitIntoWords(const std::string& text);
 
 template <typename StringContainer>
-std::set<std::string, std::less<>> MakeUniqueNonEmptyStrings(StringContainer& container) { 
-std::set<std::string, std::less<>> non_empty_strings;
-    if constexpr(std::is_convertible_v<StringContainer,
-                                       std::string_view>)
-    for (auto const& word : SplitIntoWords(container))
-        non_empty_strings.insert(std::string(word));
-    else 
-    for (auto const& word : container)
-        non_empty_strings.insert(std::string(word));
+std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    std::set<std::string> non_empty_strings;
+    for (const std::string& str : strings) {
+        if (!str.empty()) {
+            non_empty_strings.insert(str);
+        }
+    }
     return non_empty_strings;
 }
