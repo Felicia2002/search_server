88df2a4483a6012b58876c61168b1da7fa5d7335
Надежда Чернигина <97780261+Felicia2002@users.noreply.github.com>
2023-01-21 12:09:44 +0300

    Update request_queue.cpp

--- request_queue.cpp
+++ request_queue.cpp
@@ -1,20 +1,20 @@
 #include "request_queue.h"
 
-RequestQueue::RequestQueue(const SearchServer& search_server)
-    :search_server_(search_server)
-{}
+using std::string;
+using std::vector;
 
-int RequestQueue::GetNoResultRequests() const {
-    return requests_.size();
+RequestQueue::RequestQueue(const SearchServer& search_server) : search_server_(search_server) {}
+
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query, DocumentStatus status) {
+    return AddFindRequest(raw_query, [status](int document_id, DocumentStatus document_status, int rating) {
+        return document_status == status;
+        });
 }
 
-std::vector<Document> RequestQueue::AddFindRequest(
-    const std::string& raw_query,
-    DocumentStatus status) {
-    return AddFindRequest(raw_query,
-           [status](int document_id,
-                    DocumentStatus document_status,
-                    int rating) {
-                        return document_status == status;
-                    });
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query) {
+    return AddFindRequest(raw_query, DocumentStatus::ACTUAL);
+}
+
+uint64_t RequestQueue::GetNoResultRequests() const {
+    return number_empty_answer_;
 }
