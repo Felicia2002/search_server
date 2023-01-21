0b3a85105eaf3aaa6cae9a1413c33c33ee8b1a7d
Надежда Чернигина <97780261+Felicia2002@users.noreply.github.com>
2023-01-21 12:12:03 +0300

    Update string_processing.cpp

--- string_processing.cpp
+++ string_processing.cpp
@@ -3,23 +3,23 @@
 using std::string;
 using std::vector;
 
-
-#include "string_processing.h"
-
-std::vector<std::string_view>
-    SplitIntoWords(std::string_view str) {
-    std::vector<std::string_view> result;
-    const int64_t pos_end = str.npos;
-    while (true) {
-        int64_t space = str.find(' ');
-        result.push_back(space == pos_end ? str.substr() :
-                         str.substr(0, space));
-        if (space == pos_end) {
-            break;
-        } else {
-            ++space;
-            str.remove_prefix(space);
+vector<string> SplitIntoWords(const string& text) {
+    vector<string> words;
+    string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        }
+        else {
+            word += c;
         }
     }
-    return result;
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+
+    return words;
 }
